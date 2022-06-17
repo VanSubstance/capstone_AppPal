302b6f4431c22fbbb2774b169a282caa3ca1e8fd
Kim-Jong-Gyu <62927374+Kim-Jong-Gyu@users.noreply.github.com>
2022-06-18 00:53:57 +0900

    Add(Application): #40
    + Change Python Server Url

--- application/app/src/main/java/com/example/apppal/Utils.java
+++ application/app/src/main/java/com/example/apppal/Utils.java
@@ -3,7 +3,7 @@ package com.example.apppal;
 import com.example.apppal.VO.GestureType;
 
 public class Utils {
-    public final static String PYTHON_SERVER_URL = "124.197.210.234";
+    public final static String PYTHON_SERVER_URL = "ec2-54-180-30-60.ap-northeast-2.compute.amazonaws.com";
     public final static int GESTURE_SOCKET_PORT = 4000;
     public final static GestureType MENU_GESTURE = GestureType.MASK;
 
