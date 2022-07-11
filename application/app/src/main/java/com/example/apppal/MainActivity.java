20a8eb13fd94fdad45d22f1aa74dcb9f797d05b8
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2022-07-11 21:38:55 +0900

    feat(Detaching Activity)
    #44
    
    ~ 메인 ~ 카메라 액티비티 분리

--- application/app/src/main/java/com/example/apppal/MainActivity.java
+++ application/app/src/main/java/com/example/apppal/MainActivity.java
@@ -1,242 +1,42 @@
 package com.example.apppal;
 
-import android.graphics.Bitmap;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Handler;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.Toast;
 
-// ContentResolver dependency
-import com.example.apppal.Storage.GlobalState;
-import com.google.mediapipe.formats.proto.LandmarkProto.Landmark;
-import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmark;
-import com.google.mediapipe.solutioncore.CameraInput;
-import com.google.mediapipe.solutioncore.SolutionGlSurfaceView;
-import com.google.mediapipe.solutions.hands.HandLandmark;
-import com.google.mediapipe.solutions.hands.Hands;
-import com.google.mediapipe.solutions.hands.HandsOptions;
-import com.google.mediapipe.solutions.hands.HandsResult;
-
-/**
- * Main activity of MediaPipe Hands app.
- */
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-
-    private Hands hands;
-    // Run the pipeline and the model inference on GPU or CPU.
-    private static final boolean RUN_ON_GPU = true;
-
-    private enum InputSource {
-        UNKNOWN,
-        CAMERA,
-    }
-
-    private InputSource inputSource = InputSource.UNKNOWN;
-
-    // Live camera demo UI and camera components.
-    private CameraInput cameraInput;
-
-    private SolutionGlSurfaceView<HandsResult> glSurfaceView;
-    private GestureRecognitionSocket gestureSocket;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setupLiveDemoUiComponents();
-        checkArCompatibility();
-        setupAREnvironment();
+        setUpPlayStartbutton();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (inputSource == InputSource.CAMERA) {
-            // Restarts the camera and the opengl surface rendering.
-            cameraInput = new CameraInput(this);
-            cameraInput.setNewFrameListener(textureFrame -> hands.send(textureFrame));
-            glSurfaceView.post(this::startCamera);
-            glSurfaceView.setVisibility(View.VISIBLE);
-        } else {
-        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (inputSource == InputSource.CAMERA) {
-            glSurfaceView.setVisibility(View.GONE);
-            cameraInput.close();
-        } else {
-        }
     }
 
     /**
      * Sets up the UI components for the live demo with camera input.
      */
-    private void setupLiveDemoUiComponents() {
+    private void setUpPlayStartbutton() {
         Button startCameraButton = findViewById(R.id.button_start_drawing);
         startCameraButton.setOnClickListener(
                 v -> {
-                    if (inputSource == InputSource.CAMERA) {
-                        return;
-                    }
-                    stopCurrentPipeline();
-                    setupStreamingModePipeline(InputSource.CAMERA);
-                    initializeConnection();
+                    Intent playIntent = new Intent(MainActivity.this, PlayActivity.class);
+                    startActivity(playIntent);
+                    finish();
                 });
-    }
-
-    private void setupAREnvironment() {
-        Button startArCoreButton = findViewById(R.id.button_start_arcore);
-        startArCoreButton.setOnClickListener(
-                v -> {
-                    if (inputSource == InputSource.CAMERA) {
-                        return;
-                    }
-                    stopCurrentPipeline();
-                    setupStreamingModePipeline(InputSource.CAMERA);
-                }
-        );
-    }
-
-    /**
-     * Sets up core workflow for streaming mode.
-     */
-    private void setupStreamingModePipeline(InputSource inputSource) {
-        this.inputSource = inputSource;
-        // Initializes a new MediaPipe Hands solution instance in the streaming mode.
-        hands =
-                new Hands(
-                        this,
-                        HandsOptions.builder()
-                                .setStaticImageMode(false)
-                                .setMaxNumHands(1)
-                                .setRunOnGpu(RUN_ON_GPU)
-                                .build());
-        hands.setErrorListener((message, e) -> Log.e(TAG, "MediaPipe Hands error:" + message));
-
-        if (inputSource == InputSource.CAMERA) {
-            cameraInput = new CameraInput(this);
-            cameraInput.setNewFrameListener(textureFrame -> hands.send(textureFrame));
-        } else {
-        }
-
-        // Initializes a new Gl surface view with a user-defined HandsResultGlRenderer.
-        glSurfaceView =
-                new SolutionGlSurfaceView<>(this, hands.getGlContext(), hands.getGlMajorVersion());
-        glSurfaceView.setSolutionResultRenderer(new HandsResultGlRenderer());
-        glSurfaceView.setRenderInputImage(true);
-        hands.setResultListener(
-                handsResult -> {
-                    logWristLandmark(handsResult, /*showPixelValues=*/ false);
-                    glSurfaceView.setRenderData(handsResult);
-                    glSurfaceView.requestRender();
-                });
-
-        // The runnable to start camera after the gl surface view is attached.
-        // For video input source, videoInput.start() will be called when the video uri is available.
-        if (inputSource == InputSource.CAMERA) {
-            glSurfaceView.post(this::startCamera);
-        }
-
-        // Updates the preview layout.
-        FrameLayout frameLayout = findViewById(R.id.preview_display_layout);
-        frameLayout.removeAllViewsInLayout();
-        frameLayout.addView(glSurfaceView);
-        glSurfaceView.setVisibility(View.VISIBLE);
-        frameLayout.requestLayout();
-    }
-
-    private void startCamera() {
-        cameraInput.start(
-                this,
-                hands.getGlContext(),
-                CameraInput.CameraFacing.BACK,
-                glSurfaceView.getWidth(),
-                glSurfaceView.getHeight());
-    }
-
-    private void stopCurrentPipeline() {
-        if (cameraInput != null) {
-            cameraInput.setNewFrameListener(null);
-            cameraInput.close();
-        }
-        if (glSurfaceView != null) {
-            glSurfaceView.setVisibility(View.GONE);
-        }
-        if (hands != null) {
-            hands.close();
-        }
-    }
-
-    private void logWristLandmark(HandsResult result, boolean showPixelValues) {
-        if (result.multiHandLandmarks().isEmpty()) {
-            return;
-        }
-        NormalizedLandmark wristLandmark =
-                result.multiHandLandmarks().get(0).getLandmarkList().get(HandLandmark.WRIST);
-        // For Bitmaps, show the pixel values. For texture inputs, show the normalized coordinates.
-        if (showPixelValues) {
-            int width = result.inputBitmap().getWidth();
-            int height = result.inputBitmap().getHeight();
-            Log.i(
-                    TAG,
-                    String.format(
-                            "MediaPipe Hand wrist coordinates (pixel values): x=%f, y=%f",
-                            wristLandmark.getX() * width, wristLandmark.getY() * height));
-        } else {
-            Log.i(
-                    TAG,
-                    String.format(
-                            "MediaPipe Hand wrist normalized coordinates (value range: [0, 1]): x=%f, y=%f",
-                            wristLandmark.getX(), wristLandmark.getY()));
-        }
-        if (result.multiHandWorldLandmarks().isEmpty()) {
-            return;
-        }
-        Landmark wristWorldLandmark =
-                result.multiHandWorldLandmarks().get(0).getLandmarkList().get(HandLandmark.WRIST);
-        Log.i(
-                TAG,
-                String.format(
-                        "MediaPipe Hand wrist world coordinates (in meters with the origin at the hand's"
-                                + " approximate geometric center): x=%f m, y=%f m, z=%f m",
-                        wristWorldLandmark.getX(), wristWorldLandmark.getY(), wristWorldLandmark.getZ()));
-    }
-
-    private void initializeConnection() {
-        gestureSocket = new GestureRecognitionSocket();
-        gestureSocket.start();
-
-        GlobalState.textAnnounce = findViewById(R.id.text_announce);
-        Utils.IS_GESTURE_DETECTION = true;
-    }
-
-    private void checkArCompatibility() {
-//        ArCoreApk.Availability availability = ArCoreApk.getInstance().checkAvailability(this);
-//        if (availability.isTransient()) {
-//            // Continue to query availability at 5Hz while compatibility is checked in the background.
-//            new Handler().postDelayed(new Runnable() {
-//                @Override
-//                public void run() {
-//                    checkArCompatibility();
-//                }
-//            }, 200);
-//        }
-//        if (!availability.isSupported()) {
-//            Toast.makeText(this, "해당 앱을 실행할 수 없습니다.\n앱을 종료해주세요.", Toast.LENGTH_SHORT);
-//            moveTaskToBack(true);
-//            finishAndRemoveTask();
-//            android.os.Process.killProcess(android.os.Process.myPid());
-//        }
     }
 }
 
