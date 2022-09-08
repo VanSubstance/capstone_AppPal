7ef4cfbd1c23fc1f948ee957f20d9277fc66e603
Kim-Jong-Gyu <62927374+Kim-Jong-Gyu@users.noreply.github.com>
2022-09-08 20:11:40 +0900

    feat(AWS Connection)
    #71
    ~ change URL

--- application_new/app/src/main/java/com/capstone/apppal/utils/GlobalState.java
+++ application_new/app/src/main/java/com/capstone/apppal/utils/GlobalState.java
@@ -9,9 +9,9 @@ import java.util.ArrayList;
 
 public class GlobalState {
   public static ArrayList<ArrayList<CoordinateInfo>> gestureTrackings = new ArrayList<>();
-  public final static String PYTHON_SERVER_URL = "124.197.210.234";
+  public final static String PYTHON_SERVER_URL = "ec2-3-38-253-205.ap-northeast-2.compute.amazonaws.com";
   public final static int GESTURE_SOCKET_PORT = 4000;
-  public final static GestureType MENU_GESTURE = GestureType.MASK;
+  public final static GestureType MENU_GESTURE = GestureType.FIVE;
   public static InputStreamReader is;
   public static OutputStreamWriter os;
   public static GestureType currentGesture;
