ba39fa40503c1147b7ea265a671a3fb4180c6c37
Kim-Jong-Gyu <62927374+Kim-Jong-Gyu@users.noreply.github.com>
2022-09-26 23:46:42 +0900

    add(Checking RoomCode)
    #89
    
    + roomcode 체크 함수 생성
    + invite 생성 함수 생성
    ~ 파이어베이스 초기화 해결

--- application_new/app/src/main/java/com/capstone/apppal/RoomHandler.java
+++ application_new/app/src/main/java/com/capstone/apppal/RoomHandler.java
@@ -1,8 +1,12 @@
 package com.capstone.apppal;
 
 import com.capstone.apppal.VO.RoomsInfo;
+import com.capstone.apppal.utils.GlobalState;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -13,23 +17,29 @@ public class RoomHandler {
   protected boolean isPairing = false;
   private static final String ROOT_FIREBASE_ROOMS = "rooms";
   private String roomcode;
+  DatabaseReference roomsListRef = databaseReference.child(ROOT_FIREBASE_ROOMS);
 
   public void singleRoomCreate(RoomsInfo roomsInfo){
     if(isPairing == false) {
-      makeRoomCode();
-      DatabaseReference roomsListRef = databaseReference.child(ROOT_FIREBASE_ROOMS);
-      roomsListRef.setValue(roomcode);
+      while(true){
+        makeRoomCode();
+        if(checkingRoomCode(roomcode)){
+          break;
+        }
+      }
+      GlobalState.inviteCode = makeInviteCode();
       Long timestamp = System.currentTimeMillis();
       roomsInfo.setTimestamp(timestamp);
       roomsListRef.child(roomcode).setValue(roomsInfo);
     }
   }
-  public void makeRoomCode(){
+
+  public String makeRoomCode(){
     roomcode = "";
     ArrayList<Integer> numcode = new ArrayList();
     ArrayList<Character> engcode = new ArrayList();
     for(int i = 0; i < 3; i++){
-      numcode.add((int)Math.random() * 10);
+      numcode.add((int)(Math.random() * 10));
     }
     for(int i = 0; i < 3; i++){
       engcode.add((char)((Math.random() * 26) +65));
@@ -38,5 +48,41 @@ public class RoomHandler {
       roomcode = roomcode.concat(String.valueOf(numcode.get(i)));
       roomcode = roomcode.concat(String.valueOf(engcode.get(i)));
     }
+    return roomcode;
+  }
+  public String makeInviteCode(){
+    String invite ="";
+    ArrayList<Integer> numcode = new ArrayList();
+    ArrayList<Character> engcode = new ArrayList();
+    for(int i = 0; i < 2; i++){
+      numcode.add((int)(Math.random() * 10));
+    }
+    for(int i = 0; i < 2; i++){
+      engcode.add((char)((Math.random() * 26) +65));
+    }
+    for(int i = 0; i <2; i++){
+      invite = invite.concat(String.valueOf(numcode.get(i)));
+      invite = invite.concat(String.valueOf(engcode.get(i)));
+    }
+    return invite;
+  }
+  public boolean checkingRoomCode(String code){
+    final boolean[] checking = {true};
+    roomsListRef.addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+          if (snapshot.getValue().equals(code)) {
+            checking[0] = false;
+          } else {
+
+          }
+        }
+      }
+      @Override
+      public void onCancelled(DatabaseError databaseError) {
+      }
+    });
+    return checking[0];
   }
 }
