19306272d3cd43b521090977baf06f7e5a72ecb2
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2022-10-05 14:28:42 +0900

    100 multiplay without sharing physical space (#101)
    * remove(Multiplaying in physical)
    
    #100
    
    - 물리공간 공유 멀티플레이 코드 삭제
    // 방 진입 시 기존 lines 초기화 문제 발생
    
    * feat(Clearing Drawing at the enterance)
    
    #100
    
    ~ 방 진입 시 이전 그림 삭제되는 버그 수정
    
    * feat(Login Loading)
    
    #100
    
    ~ 로그인 중간에 취소 -> 로딩 아이콘 숨기기
    ~ 멀티플레이 작동 확인

--- application_new/app/src/main/java/com/capstone/apppal/OnBoardingActivity.java
+++ application_new/app/src/main/java/com/capstone/apppal/OnBoardingActivity.java
@@ -4,35 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.capstone.apppal.VO.RoomsInfo;
-import com.capstone.apppal.VO.UserInfo;
 import com.capstone.apppal.utils.GlobalState;
 import com.capstone.apppal.view.fragments.ListFragment;
 import com.capstone.apppal.view.fragments.LoginFragment;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
 
@@ -102,10 +83,6 @@ public class OnBoardingActivity extends AppCompatActivity {
     finishLoading();
   }
 
-  /**
-   * @param roomInfo
-   * @구현 단순히 진입이 아닌 타겟 방 진입:: 신규도 마찬가지임
-   */
   public void enterDrawingRoom(RoomsInfo roomInfo) {
     GlobalState.currentRoomInfo = roomInfo;
     roomHandler.getStrokeInfo(roomInfo.getRoomCode(), data -> {
